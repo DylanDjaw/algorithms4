67bbe97dc64e41d53c67f3b4f19a38713f378a50
Dylan_homePC <jiaoyongnian@foxmail.com>
2021-08-18 20:34:13 +0800

    change function name

--- a_1_5_union_find.cc
+++ a_1_5_union_find.cc
@@ -15,9 +15,9 @@ public:
         for(int i = 0; i < N; ++i)
             _id[i] = i;
     }
-    void union1(int p, int q);
-    int find1(int p);
-    bool connected1(int p, int q){   return find1(p) == find1 (q); }
+    void union(int p, int q);
+    int find(int p);
+    bool connected(int p, int q){   return find(p) == find (q); }
     int count(){    return _count;   }
 
 private:
@@ -26,12 +26,12 @@ private:
     int *_id;           //分量id（以触点为索引）
 };
 
-int UF::find1(int p){
+int UF::find(int p){
     return _id[p];
 }
-void UF::union1(int p, int q){
+void UF::union(int p, int q){
     //若p, q相连，则返回
-    if(connected1(p, q))
+    if(connected(p, q))
         return;
     //否则将与p在同一联通分量中的点都与q相连
     int pId = _id[p];
@@ -47,7 +47,7 @@ void UF::union1(int p, int q){
 
 int main(){
     ifstream input;
-    input.open("/home/jyn/workspace/algorithm4/input/tinyUF.txt", ios::in | ios::in);
+    input.open("/home/jyn/workspace/algorithm4/input/tinyUF.txt", ios::in);
     if(input.is_open()) cout << "success" << endl;
     int n;
     input >> n;
@@ -58,9 +58,9 @@ int main(){
         input >> q;
         //cout << p << "\t" << q << endl;
         //若不联通，则联通，若联通，则忽略
-        if( ! u.connected1(p, q) )
-            u.union1(p, q);            
+        if( ! u.connected(p, q) )
+            u.union(p, q);            
     }
-    cout << u.connected1(4,8) << endl;
+    cout << u.connected(4,8) << endl;
     cout << u.count() << endl;
 }
