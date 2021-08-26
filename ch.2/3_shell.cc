afe6d12c1ec4c2d8ee79da58fa1cc71563dc994f
WorkPC <jiaoyongnian@foxmail.com>
2021-08-26 18:01:36 +0800

    part of 3_shell.cc

---
+++ ch.2/3_shell.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <stack>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+
+void sort(int *a, int n);
+void exch(int &p, int &q){ int temp = p; p = q; q = temp; }
+void show(int *a, int n){
+    for(int i = 0; i < n; ++i)
+        cout << a[i] << endl;
+}
+bool isSorted(int *a, int n){
+    for(int i = 1; i < n; ++i)
+        if(a[i - 1] > a[i])
+            return 0;
+    return 1;            
+}
+
+void sort(int *a, int n){
+    if(nullptr == a || 0 == n){
+        cout << "wrong input" << endl;
+        return;
+    }
+    int h;
+    while(h < 3/n)
+        h = 3*h + 1;
+    while(h > 0){
+        
+        for(int i = h; i < n; i += h){
+            int j;
+            int num = a[j];
+            for(j = i; a[j] > num && j > 0; j -= h)
+                a[j] = a[j-h];
+        }
+        h /= 3;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    srand((unsigned)time(NULL));
+    for(int i = 0; i < n; ++i)
+        a[i] = rand();
+    sort(a, n);
+    if(isSorted(a, n))
+        show(a, n);
+}
